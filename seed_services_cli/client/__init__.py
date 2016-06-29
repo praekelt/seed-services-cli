bf5870d3792bd3c98d7ecf5b88e7ab474c799a84
Mike Jones <mike@westerncapelabs.com>
2016-06-29 17:47:28 +0200

    remove stray version

--- seed_services_cli/client/__init__.py
+++ seed_services_cli/client/__init__.py
@@ -3,8 +3,6 @@
 from .identity_store import IdentityStoreApiClient
 from .stage_based_messaging import StageBasedMessagingApiClient
 
-__version__ = "0.0.2"
-
 __all__ = [
     'IdentityStoreApiClient', 'StageBasedMessagingApiClient'
 ]
