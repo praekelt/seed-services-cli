46db4cbd1ebe45c4fff8558c543df7f7ef8a5b3c
Mike Jones <mike@westerncapelabs.com>
2016-08-17 17:45:16 +0200

    Add CI commands

--- seed_services_cli/main.py
+++ seed_services_cli/main.py
@@ -6,6 +6,7 @@ import seed_services_cli.identity_store
 import seed_services_cli.stage_based_messaging
 import seed_services_cli.hub
 import seed_services_cli.auth
+import seed_services_cli.ci
 
 
 class config(object):
@@ -26,11 +27,15 @@ class config(object):
         password = 'REPLACEME'
         api_url = 'http://auth.example.org'
 
+    class ci(object):
+        api_url = 'http://ci-svc.example.org/api/v1'
+        token = 'REPLACEME'
+
 
 @click.group(name="seed-services-cli")
 @click.version_option()
 @click_config.wrap(module=config, sections=('hub', 'identity_store',
-                   'stage_based_messaging', 'auth'))
+                   'stage_based_messaging', 'auth', 'ci'))
 @click.pass_context
 def cli(ctx):
     """ Seed Services command line utility. """
@@ -47,3 +52,5 @@ cli.command('sbm-messages-import')(seed_services_cli.stage_based_messaging.messa
 cli.command('hub-registrations-import')(seed_services_cli.hub.registrations_import)  # noqa
 cli.command('auth-user-add')(seed_services_cli.auth.user_add)  # noqa
 cli.command('auth-user-add-team')(seed_services_cli.auth.user_add_team)  # noqa
+cli.command('ci-status')(seed_services_cli.ci.status)  # noqa
+cli.command('ci-user-token-generate')(seed_services_cli.ci.user_tokens_generate)  # noqa
