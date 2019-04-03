943fca5e70be086d4f29fd580103d7647a81f99a
Rudi Giesler <rudi@praekelt.org>
2019-04-03 11:29:57 +0200

    Release 0.0.13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="seed-services-cli",
-    version="0.0.12",
+    version="0.0.13",
     url='http://github.com/praekelt/seed-services-cli',
     license='BSD',
     description="A command-line interface for Seed Services HTTP APIs",
